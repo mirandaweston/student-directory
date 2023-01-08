9d028006c260d03edc092c1a92f75093d20ddfec
mirandaweston <mweston105@yahoo.co.uk>
2023-01-08 16:48:13 +0000

    Loading info from the file on startup and using STDIN.gets to take input from standard input

--- directory.rb
+++ directory.rb
@@ -1,25 +1,5 @@
 @students = []
 
-def input_students
-  puts "Please enter the names of the students"
-  puts "To finish, just hit return twice"
-  
-  name = gets.chomp
-  
-  while !name.empty? do
-    @students << {name: name, cohort: :november}
-    puts "Now we have #{@students.count} students"
-    name = gets.chomp
-  end
-end
-
-def interactive_menu
-  loop do
-    print_menu
-    process(gets.chomp)
-  end
-end
-
 def print_menu
   puts "1. Input the students"
   puts "2. Show the students"
@@ -28,10 +8,26 @@ def print_menu
   puts "9. Exit"
 end
 
-def show_students
-  print_header
-  print_student_list
-  print_footer
+def input_students
+  puts "Please enter the names of the students"
+  puts "To finish, just hit return twice"
+
+  name = STDIN.gets.chomp
+
+  while !name.empty? do
+  
+    @students << {name: name, cohort: :november}
+    puts "Now we have #{@students.count} students"
+  
+    name = STDIN.gets.chomp
+  end
+end
+
+def interactive_menu
+  loop do
+    print_menu
+    process(STDIN.gets.chomp)
+  end
 end
 
 def process(selection)
@@ -40,13 +36,21 @@ def process(selection)
     input_students
   when "2"
     show_students
-  when "3"
-    save_students
   when "9"
     exit
+  when "3"
+    save_students
+  when "4"
+    load_students
   else
     puts "I don't know what you meant, try again"
   end
+end
+
+def show_students
+  print_header
+  print_student_list
+  print_footer
 end
 
 def print_header
@@ -65,7 +69,9 @@ def print_footer
 end
 
 def save_students
+
   file = File.open("students.csv", "w")
+  
   @students.each do |student|
     student_data = [student[:name], student[:cohort]]
     csv_line = student_data.join(",")
@@ -74,13 +80,26 @@ def save_students
   file.close
 end
 
-def load_students
-  file = File.open("students.csv", "r")
+def load_students(filename = "students.csv")
+  file = File.open(filename, "r")
   file.readlines.each do |line|
-  name, cohort = line.chomp.split(",")
+  name, cohort = line.chomp.split(',')
     @students << {name: name, cohort: cohort.to_sym}
   end
   file.close
 end
 
+def try_load_students
+  filename = ARGV.first
+  return if filename.nil?
+  if File.exists?(filename)
+    load_students(filename)
+     puts "Loaded #{@students.count} from #{filename}"
+  else
+    puts "Sorry, #{filename} doesn't exist."
+    exit
+  end
+end
+
+try_load_students
 interactive_menu
